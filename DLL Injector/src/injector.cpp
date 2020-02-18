8e4c2bacacb53127a357297c1a8cad4165a78fef
Brenden <brenden111@hotmail.com>
2020-02-17 20:28:03 -0500

    removed clutter from std::cout for base addr

--- DLL Injector/src/injector.cpp
+++ DLL Injector/src/injector.cpp
@@ -24,32 +24,36 @@ int main(int argc, char* argv[])
 		cerr << "[ERROR] The process specified was not found." << std::endl;
 		return -1;
 	}
+	//Check if the path to the file exists
 	if (!PathFileExistsA(d)) {
 		cerr << "\n[ERROR] Invalid file path specified." << endl;
 		return -1;
 	}
 	else {
+		//open a handle to the target process
 		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, obj.getProcessIDbyName(p));
 		if (hProcess == NULL) {
 			cerr << "Could not open process" << GetLastErrorAsString();
 			return -1;
 		}
 		else {
+			//allocate memory in process, save base address
 			LPVOID dllP = VirtualAllocEx(hProcess, 0, strlen(d) + 1, MEM_COMMIT, PAGE_READWRITE);
 			if (dllP == NULL) {
 				cerr << "Failed to allocate memory in process. " << GetLastErrorAsString() << endl;
 				return -1;
 			}
 			else {
+				//write the path to the region allocated
 				WriteProcessMemory(hProcess, dllP, (LPVOID)d, strlen(d) + 1, 0);
+				//create thread that loads dll
 				HANDLE handleLthd = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA"), dllP, 0, 0);
 				DWORD retVal = WaitForSingleObject(handleLthd, INFINITE);
 				if (handleLthd == NULL | retVal == WAIT_FAILED ) {
 					cerr << "Failed to create remote thread in process." << endl;
 					return -1;
 				}
-				std::cout << std::endl << "DLL path allocated at address: " << dllP << std::endl;
-				std::cin.get();
+				printf("Path allocation address:  0x%08x\n", dllP);
 				VirtualFreeEx(hProcess, dllP, strlen(d) + 1, MEM_RELEASE);
 				return 0;
 			}
